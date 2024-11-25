2a7651a6f5fb60ef8a530624d0ef8aa0f244ea31
leorasdsouza <leorastanak@gmail.com>
2024-11-25 18:53:36 +0530

    Second Commit

--- code/src/config.ts
+++ code/src/config.ts
@@ -4,5 +4,5 @@ dotenv.config();
 export const config = {
     DEVREV_PAT: process.env.DEVREV_PAT || '',
     SLACK_BOT_TOKEN: process.env.SLACK_BOT_TOKEN || '',
-    SLACK_CHANNEL: process.env.SLACK_CHANNEL || '#sprint-summarizer',  // Adjust to your Slack channel
+    SLACK_CHANNEL: process.env.SLACK_CHANNEL || '#sprint-summarizer',  
 };
